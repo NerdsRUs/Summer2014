a8445133a3e8150db7aeb2ce3cdab7086d9ee94b
Adam Pettyjohn <imposter110@gmail.com>
2014-06-28 19:29:05 -0700

    go away stupid shit

--- ZZZ/Assets/Source/Client/Script.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Script : MonoBehaviour 
-{
-	public void Init(EventManager eventManager, int objectID)
-	{
-	}
-}
--- ZZZ/Assets/Source/Common/StaticData/DataManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DataManager
-{
-	static public void LoadData()
-	{
-	}
-}
