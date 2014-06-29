6b0c597fbc8cac6c6b5ccba45171e9954d9caf03
Sharsnik <sharsnik@gmail.com>
2014-06-28 18:06:51 -0700

    Adding event system

--- ZZZ/Assets/Source/Common/Engine/EventSystem/EventManger.cs
+++ ZZZ/Assets/Source/Common/Engine/EventSystem/EventManger.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EventManger : MonoBehaviour {
+public class EventManger : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	public delegate void EventCall();
+
+	float mTime;
+	Vector3 mPosition;
+	EventCall mAction;
+
+	static bool mExecutingEvent;
+
+	public void EventManger(EventCall eventCall, float time, Vector3 position)
+	{
+		mTime = time;
+		mPosition = position;
+		mAction = eventCall;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void execute(EngineInstance instance)
+	{
+		EventAPI.setCurrentInstance(instance);
+		mExecutingEvent = true;
+
+		try
+		{
+			mAction();
+		}
+		catch (Exception e)
+		{
+			Debug.LogError("Event had error: " + e.Message + "/n" + e.StackTrace);
+		}
+
+		mExecutingEvent = false;
+	}
+
+	public string getCommandString()
+	{
+		return mAction.Method.Name;
+	}
+
+	public string getName()
+	{
+		return mTime.ToString("0.00") + ") " + mAction.Method.Name;
+	}
+
+	static public bool isExecutingEvent()
+	{
+		return mExecutingEvent;
 	}
 }
